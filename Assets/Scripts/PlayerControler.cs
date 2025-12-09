7c86cc229b400b25690b1d072f6ade30198c8777
MParrillaL <manuelparrillalahoz@gmail.com>
2025-12-09 19:42:15 +0100

    flip player

--- Assets/Scripts/PlayerControler.cs
+++ Assets/Scripts/PlayerControler.cs
@@ -2,8 +2,9 @@ using UnityEngine;
 
 public class PlayerControler : MonoBehaviour
 {
-    [SerializeField] 
-    private float speed;       // ahora sí la podrás cambiar en el Inspector
+    [SerializeField]
+    private float speed;
+    private int direction = 1;
 
     private Rigidbody2D m_rigidbody2D;
     private GatherInput m_gatherinput;
@@ -11,19 +12,30 @@ public class PlayerControler : MonoBehaviour
 
     private void Awake()
     {
-        // Coger referencias a los componentes del mismo GameObject
         m_gatherinput = GetComponent<GatherInput>();
         m_transform = GetComponent<Transform>();
         m_rigidbody2D = GetComponent<Rigidbody2D>();
     }
 
-    private void FixedUpdate()
+    private void Move()
     {
         if (m_rigidbody2D == null || m_gatherinput == null) return;
-
-        // Mantener la velocidad vertical actual
-        Vector2 vel = m_rigidbody2D.linearVelocity; // o .velocity según tu versión
+        Flip();
+        Vector2 vel = m_rigidbody2D.linearVelocity; 
         vel.x = speed * m_gatherinput.ValueX;
-        m_rigidbody2D.linearVelocity = vel; // o .velocity = vel;
+        m_rigidbody2D.linearVelocity = vel; 
+    }
+
+    private void FixedUpdate()
+    {        Move();
+    }
+
+    private void Flip()
+    {
+        if(m_gatherinput.ValueX * direction < 0)
+        {
+            m_transform.localScale = new Vector3(-m_transform.localScale.x, 1, 1);
+            direction *= -1;
+        }
     }
 }
